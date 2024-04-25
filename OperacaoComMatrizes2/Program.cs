6b8e2f5ddc099dfb6cda5a10fd868b1d5a8ee087
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-25 11:47:21 -0300

    refactor: Declaracao de funcoes e menu interativo

--- OperacaoComMatrizes2/Program.cs
+++ OperacaoComMatrizes2/Program.cs
@@ -1,50 +1,152 @@
-﻿int tamanhoLinha = 10, tamanhoColuna = 10;
-int somaDiagonal = 0;
-int[] somaLinhas = new int[tamanhoLinha], somaColunas = new int[tamanhoLinha]; 
-int[,] matriz = new int[tamanhoLinha, tamanhoColuna];
+﻿int lineLength = 0, columnLength = 0, diagonalSum = 0, reverseDiagonalSum = 0;
+int[] sumLineResults = new int[lineLength], sumColumnResults = new int[lineLength]; 
+int[,] mainMatrix = new int[lineLength, columnLength];
 
-for (int linha = 0; linha < tamanhoLinha; linha++)
+void getMatrixLengths()
 {
-    for (int coluna = 0; coluna < tamanhoColuna; coluna++)
+    do
     {
-        matriz[linha, coluna] = new Random().Next(0, 11);
-    }
-}
-
-for (int linha = 0; linha < tamanhoLinha; linha++)
-{
-    for (int coluna = 0; coluna < tamanhoColuna; coluna++)
-    {
-        somaLinhas[linha] += matriz[linha, coluna];
-        somaColunas[linha] += matriz[coluna, linha];
-        if (linha == coluna)
+        Console.Write("Selecione quantidade de linhas das matrizes: ");
+        lineLength = int.Parse(Console.ReadLine());
+        Console.Write("Selecione quantidade de colunas das matrizes: ");
+        columnLength = int.Parse(Console.ReadLine());
+        
+        if (lineLength < 1 || columnLength < 1)
         {
-            somaDiagonal += matriz[linha, coluna];
+            Console.WriteLine("Os valores devem ser maiores que 0. Tente novamente.\n");
         }
-    }
+        else
+        {
+            Console.Clear();
+        }
+
+    } while (lineLength < 1 || columnLength < 1);
 }
 
-
-Console.WriteLine("\nMatriz gerada: \n");
-for (int linha = 0; linha < tamanhoLinha; linha++)
+void printMatrix(int[,] matrix, string header)
 {
-    for (int coluna = 0; coluna < tamanhoColuna; coluna++)
+    Console.WriteLine($"{header}\n");
+    for (int line = 0; line < lineLength; line++)
     {
-        Console.Write($"{matriz[linha, coluna]:00} ");
+        for (int column = 0; column < columnLength; column++)
+        {
+            Console.Write($"{matrix[line, column]:00} ");
+        }
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-Console.WriteLine("\nSoma das linhas: \n");
-for (int i = 0; i < tamanhoLinha; i++)
+void printResults(int[] results, int resultsLength, string header) 
 {
-    Console.WriteLine($"linha {i + 1}: {somaLinhas[i]}");
+    Console.WriteLine($"{header}\n");
+    for (int i = 0; i < resultsLength; i++)
+    {
+        Console.WriteLine($"linha {i + 1}: {results[i]}");
+    }
+    Console.WriteLine();
 }
 
-Console.WriteLine("\nSoma das colunas: \n");
-for (int i = 0; i < tamanhoLinha; i++)
+int[,] genRandomMatriz(int lineLength, int columnLength)
 {
-    Console.WriteLine($"coluna {i + 1}: {somaColunas[i]}");
+    int[,] matrix = new int[lineLength, columnLength];
+    for (int line = 0; line < lineLength; line++)
+    {
+        for (int column = 0; column < columnLength; column++)
+        {
+            matrix[line, column] = new Random().Next(0, 100);
+        }
+    }
+    return matrix;
 }
 
-Console.WriteLine($"\nSoma das diagonais: {somaDiagonal}");
+int[] sumMatrixLines(int[,] matrix)
+{
+    int[] results = new int[lineLength];
+    for (int line = 0; line < lineLength; line++)
+    {
+        for (int column = 0; column < columnLength; column++)
+        {
+            results[line] += matrix[line, column];
+        }
+    }
+
+    return results;
+}
+
+int[] sumMatrixColumns(int[,] matrix)
+{
+    int[] results = new int[lineLength];
+    for (int line = 0; line < lineLength; line++)
+    {
+        for (int column = 0; column < columnLength; column++)
+        {
+            results[line] += matrix[column, line];
+        }
+    }
+    return results;
+}
+
+int sumMatrixDiagonal(int[,] matrix, bool reverse = false)
+{
+    int result = 0;
+    if (reverse)
+    {
+        for (int line = 0; line < lineLength; line++)
+        {
+            for (int column = 0; column < columnLength; column++)
+            {
+                if (line + column == columnLength - 1)
+                {
+                    result += matrix[line, column];
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int line = 0; line < lineLength; line++)
+        {
+            for (int column = 0; column < columnLength; column++)
+            {
+                if (line == column)
+                {
+                    result += matrix[line, column];
+                }
+            }
+        }
+    }
+    return result;
+}
+
+do
+{
+    // Iniciar matriz para operações
+    getMatrixLengths();
+    mainMatrix = genRandomMatriz(lineLength, columnLength);
+    printMatrix(mainMatrix, "Matriz gerada:");
+
+    // Fazer operações
+    sumLineResults = sumMatrixLines(mainMatrix);
+    sumColumnResults = sumMatrixColumns(mainMatrix);
+    diagonalSum = sumMatrixDiagonal(mainMatrix);
+    reverseDiagonalSum = sumMatrixDiagonal(mainMatrix, true);
+
+    // Apresentar resultados
+    printResults(sumLineResults, lineLength, "Soma das linhas:");
+    printResults(sumColumnResults, columnLength, "Soma das colunas: ");
+    Console.WriteLine($"Soma das diagonal: {diagonalSum}");
+    Console.WriteLine($"Soma das diagonal inversa: {reverseDiagonalSum}");
+
+    Console.Write("\nDeseja repetir com outros valores? (s) Sim / (n) Não : ");
+    char input = char.Parse(Console.ReadLine());
+    if (input == 's' || input == 'S')
+    {
+        Console.Clear();
+    }
+    else
+    {
+        break;
+    }
+}
+while (true);
